89be323a4e8c03cb089df3cad9a26f13c90308c4
Sahag Arzoumanian <arzsahag@gmail.com>
2024-08-08 16:27:02 -0700

    stripe setup done

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_API_TEST_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_API_LIVE_KEY!, {
   apiVersion: "2024-06-20",
   typescript: true,
 });
